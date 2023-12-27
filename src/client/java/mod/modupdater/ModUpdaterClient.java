a0ad6e0cc06ffe2364d77b8f21141e9ca6747e55
MAK_Cpp <75676696+N4R1CK@users.noreply.github.com>
2023-12-28 02:25:42 +0300

    init commit

---
+++ src/client/java/mod/modupdater/ModUpdaterClient.java
@@ -0,0 +1,10 @@
+package mod.modupdater;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ModUpdaterClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
